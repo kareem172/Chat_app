390efab462e2ef2f846c04f6ab8f46a761ba74a6
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-06 17:53:08 +0200

    feat(db): add MongoDB connection setup to index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/ChatApp");
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 //#region importing routes
 const homeRoute = require("./routes/pages/homeRoute");
