9860f4a85fb9b49cebdcef95ad830cc866ec8909
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-06 17:54:05 +0200

    feat(models): add user model and related CRUD operations

---
+++ models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const createUser = async (name, email, password) => {
+  const user = new User({ name, email, password });
+  await user.save();
+  return user;
+};
+
+const findUserByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+const findUserById = async (id) => {
+  return await User.findById(id);
+};
+
+const isUserExist = async (email) => {
+  const user = await User.findOne({ email });
+  return user ? true : false;
+};
+
+module.exports = {
+  User,
+  createUser,
+  findUserByEmail,
+  findUserById,
+  isUserExist,
+};
