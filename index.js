3d95ecb6f842a4f7a248d40e57813922a250bf09
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-06 19:35:43 +0200

    feat: add session management and flash messages

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const dotenv = require("dotenv");
 
+//#region importing routes
+const homeRoute = require("./routes/pages/homeRoute");
+const signupRoute = require("./routes/pages/signupRoute");
+const signinRoute = require("./routes/pages/signinRoute");
+//#endregion
+
+dotenv.config();
+const app = express();
+const PORT = 5500;
+
+//#region database connection
 mongoose.connect("mongodb://127.0.0.1:27017/ChatApp");
 const db = mongoose.connection;
 
@@ -9,19 +23,20 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("Connected to MongoDB");
 });
-
-//#region importing routes
-const homeRoute = require("./routes/pages/homeRoute");
-const signupRoute = require("./routes/pages/signupRoute");
-const signinRoute = require("./routes/pages/signinRoute");
-
-const app = express();
-const PORT = 5500;
-
+//#endregion
 //#region middlewares
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(flash());
 //#endregion
 
 //#region page routes
