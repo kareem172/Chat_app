aa6431d360e0fa13d41dd68bdd4d2770ebda1fd9
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-06 17:54:32 +0200

    feat(signup): add POST route for user signup and update form action

--- routes/pages/signupRoute.js
+++ routes/pages/signupRoute.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const { createUser, findUserByEmail } = require("../../models/users");
+const { hashPassword } = require("../../utils/hash");
 router.get("/", (req, res) => {
   res.render("signup.ejs");
+});
+
+router.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+  const user = await findUserByEmail(email);
+  if (user) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+  const newUser = await createUser(
+    username,
+    email,
+    await hashPassword(password),
+  );
+
+  res.redirect("/signin");
 });
 
 module.exports = router;
