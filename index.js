aeec755a58bf6f49e44c9c0f950632b5b3397df6
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-06 15:49:18 +0200

    refactor(routes): modularize page routes into separate files

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+//#region importing routes
+const homeRoute = require("./routes/pages/homeRoute");
+const signupRoute = require("./routes/pages/signupRoute");
+const signinRoute = require("./routes/pages/signinRoute");
+
 const app = express();
 const PORT = 5500;
 
@@ -11,16 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //#endregion
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.get("/signup", (req, res) => {
-  res.render("signup.ejs");
-});
-app.get("/signin", (req, res) => {
-  res.render("signin.ejs");
-});
+//#region page routes
+app.use("/", homeRoute);
+app.use("/signup", signupRoute);
+app.use("/signin", signinRoute);
+//#endregion
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
