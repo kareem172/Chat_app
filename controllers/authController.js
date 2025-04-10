579af3a344e0a3413f645069d5384d80d7a343a0
kareem172 <kareemkhalaf1722@gmail.com>
2025-04-10 13:48:42 +0200

    feat(auth): add JWT-based authentication for signin and improve the error messages to be more ambiguity
    Implement JWT token generation and cookie storage during the signin process to replace session-based authentication. This improves security and scalability by enabling stateless authentication.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const { createUser, findUserByEmail } = require("../models/users");
 const { hashPassword } = require("../utils/hash");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 class AuthController {
   async signup(req, res) {
@@ -24,21 +25,30 @@ class AuthController {
   }
 
   async signin(req, res) {
-    const { email, password } = req.body;
-    const user = await findUserByEmail(email);
-    if (!user) {
-      req.flash("error", "User with the provided email does not exist");
+    try {
+      const { email, password } = req.body;
+      const user = await findUserByEmail(email);
+      if (!user) throw new Error("User not found");
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) throw new Error("Invalid password");
+      delete user.password;
+      const token = jwt.sign(user.toObject(), process.env.JWT_SECRET, {
+        expiresIn: "12h",
+      });
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 12 * 60 * 60 * 1000,
+      });
+      res.locals.token = token;
+      req.flash("success", "You have successfully signed in");
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      req.flash("error", "Invalid email or password");
       return res.redirect("/signin");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      req.flash("error", "Invalid password");
-      return res.redirect("/signin");
-    }
-    delete user.password;
-    req.session.user = user;
-    req.flash("success", "You have successfully signed in");
-    res.redirect("/");
   }
 }
 
